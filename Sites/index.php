c8ea10c3b50012935eac45005f49ab31702ace96
nicogeb <negebauer@uc.cl>
2015-10-29 22:43:30 -0300

    Consulta 3

--- Sites/index.php
+++ Sites/index.php
@@ -10,20 +10,6 @@
 	<h1>RENNAB</h1>
 	<h3>Realiza tu consulta favorita</h3>
 	
-	<h5>Consulta 2</h5>
-	<p>Cursos que ha aprobado un alumno</p>
-	<form action="consultasEntrega3/consulta2.php" method="post">
-	<input type="text" name="alumno"/>
-	<input type="submit"/>
-	</form><br>
-	
-	<h5>Consulta 3</h5>
-	<p>Cantidad de alumnos que cumplen los prerequisitos de un curso y no lo han tomado aun</p>
-	<form action="consultasEntrega3/consulta3.php" method="post">
-	<input type="text" name="sigla"/>
-	<input type="submit"/>
-	</form><br>
-	
 	<h5>Consulta 4</h5>
 	<p>Nota mínima, máxima, promedio y mediana de un curso dado por cada semestre dictado</p>
 	<form action="consultasEntrega3/consulta4.php" method="post">
@@ -81,6 +67,23 @@
 	echo '<label><select name="alumno">';
 
 	foreach($db -> query($datosConsulta2) as $row)
+	{
+		echo "<option value=$row[0]>$row[0]</option>";
+	}
+
+	echo '</label>';
+	echo '<input type="submit"/>';
+	echo '</form><br>';
+
+	// ***************	Consulta 3	***************
+	$datosConsulta3 = "SELECT curso.sigla FROM curso GROUP BY curso.sigla ORDER BY curso.sigla";
+	
+	echo '<h5>Consulta 3</h5>';
+	echo '<p>Alumnos que reprobaron un curso</p>';
+	echo '<form action="consultasEntrega3/consulta3.php" method="post">';
+	echo '<label><select name="sigla">';
+
+	foreach($db -> query($datosConsulta3) as $row)
 	{
 		echo "<option value=$row[0]>$row[0]</option>";
 	}
