d46750fa3471ff7c29a0b34a098525badf860ab8
nicogeb <negebauer@uc.cl>
2015-10-29 23:37:04 -0300

    Fix consulta 1

--- Sites/consultasEntrega3/consulta1.php
+++ Sites/consultasEntrega3/consulta1.php
@@ -16,14 +16,12 @@
 	
 	echo "<h1>Resultado Consulta 1:</h1>";
 	
-	$query="SELECT usuario.nombres, usuario.apellidop, usuario.apellidom
+	$query="SELECT usuario.nombres, usuario.apellidop, usuario.apellidom, curso.ano, curso.semestre
 	FROM alumno, nota, curso, usuario
 	WHERE nota.username = alumno.username
 	AND alumno.username = usuario.username
 	AND nota.notafinal <= 4.0
 	AND nota.nrc = curso.nrc
-	AND curso.ano = 2010
-	AND curso.semestre = 1
 	AND curso.sigla = '" . $_POST['sigla'] . "'";
 	
 	echo '<table border="1" class="table">';
@@ -31,6 +29,8 @@
 	echo "<th>Nombres</th>";
 	echo "<th>Apellido Paterno</th>";
 	echo "<th>Apellido Materno</th>";
+	echo "<th>Año</th>";
+	echo "<th>Semestre</th>";
 	echo "</tr>";
 	
 	foreach($db->query($query) as $row)
@@ -39,6 +39,8 @@
 		echo "<td>" . $row[0] . "</td>";
 		echo "<td>" . $row[1] . "</td>";
 		echo "<td>" . $row[2] . "</td>";
+		echo "<td>" . $row[3] . "</td>";
+		echo "<td>" . $row[4] . "</td>";
 		echo "</tr>";
 	}
 	
