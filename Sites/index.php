76a2de5c612c489611358274a2061281d8a0bcff
nicogeb <negebauer@uc.cl>
2015-10-29 22:09:09 -0300

    Added title

--- Sites/index.php
+++ Sites/index.php
@@ -1,46 +1,53 @@
+<head>
+	
+	<title>RENNAB</title>
+
+</head>
+
 <body>
-<h1>RENNAB</h1>
-<h3>Realiza tu consulta favorita</h3>
 
-<h5>Consulta 1</h5>
-<p>Alumnos que reprobaron un curso</p>
-<form action="consultasEntrega3/consulta1.php" method="post">
-<input type="text" name="sigla"/>
-<input type="submit"/>
-</form><br>
-
-<h5>Consulta 2</h5>
-<p>Cursos que ha aprobado un alumno</p>
-<form action="consultasEntrega3/consulta2.php" method="post">
-<input type="text" name="alumno"/>
-<input type="submit"/>
-</form><br>
-
-<h5>Consulta 3</h5>
-<p>Cantidad de alumnos que cumplen los prerequisitos de un curso y no lo han tomado aun</p>
-<form action="consultasEntrega3/consulta3.php" method="post">
-<input type="text" name="sigla"/>
-<input type="submit"/>
-</form><br>
-
-<h5>Consulta 4</h5>
-<p>Nota minima, maxima, promedio y mediana de un curso dado por cada semestre dictado</p>
-<form action="consultasEntrega3/consulta4.php" method="post">
-<input type="text" name="sigla"/>
-<input type="submit"/>
-</form><br>
-
-<h5>Consulta 5</h5>
-<p>Todos los profesores que ha tenido un alumno</p>
-<form action="consultasEntrega3/consulta5.php" method="post">
-<input type="text" name="alumno"/>
-<input type="submit"/>
-</form><br>
-
-<h5>Consulta 6</h5>
-<p>Promedio de notas que han obtenido los alumnos en los cursos de cada profesor</p>
-<form action="consultasEntrega3/consulta6.php" method="post">
-<input type="submit"/>
-</form><br>
+	<h1>RENNAB</h1>
+	<h3>Realiza tu consulta favorita</h3>
+	
+	<h5>Consulta 1</h5>
+	<p>Alumnos que reprobaron un curso</p>
+	<form action="consultasEntrega3/consulta1.php" method="post">
+	<input type="text" name="sigla"/>
+	<input type="submit"/>
+	</form><br>
+	
+	<h5>Consulta 2</h5>
+	<p>Cursos que ha aprobado un alumno</p>
+	<form action="consultasEntrega3/consulta2.php" method="post">
+	<input type="text" name="alumno"/>
+	<input type="submit"/>
+	</form><br>
+	
+	<h5>Consulta 3</h5>
+	<p>Cantidad de alumnos que cumplen los prerequisitos de un curso y no lo han tomado aun</p>
+	<form action="consultasEntrega3/consulta3.php" method="post">
+	<input type="text" name="sigla"/>
+	<input type="submit"/>
+	</form><br>
+	
+	<h5>Consulta 4</h5>
+	<p>Nota minima, maxima, promedio y mediana de un curso dado por cada semestre dictado</p>
+	<form action="consultasEntrega3/consulta4.php" method="post">
+	<input type="text" name="sigla"/>
+	<input type="submit"/>
+	</form><br>
+	
+	<h5>Consulta 5</h5>
+	<p>Todos los profesores que ha tenido un alumno</p>
+	<form action="consultasEntrega3/consulta5.php" method="post">
+	<input type="text" name="alumno"/>
+	<input type="submit"/>
+	</form><br>
+	
+	<h5>Consulta 6</h5>
+	<p>Promedio de notas que han obtenido los alumnos en los cursos de cada profesor</p>
+	<form action="consultasEntrega3/consulta6.php" method="post">
+	<input type="submit"/>
+	</form><br>
 
 </body>
