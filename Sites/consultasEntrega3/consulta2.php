b9c688994dad78f6abd09c3417325e28f916b1be
nicogeb <negebauer@uc.cl>
2015-10-29 21:16:33 -0300

    Consulta 1 and 2

---
+++ Sites/consultasEntrega3/consulta2.php
@@ -0,0 +1,37 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+
+<?php
+// header('Content-Type: text/html; charset=ISO-8859-1');
+try {
+	$db = new PDO("pgsql:dbname=grupo5;host=localhost;port=5432;user=grupo5;password=gruponico"); 
+	}
+catch(PDOException $e) {
+	echo $e->getMessage();
+	}
+
+echo "<h1>Resultado Consulta 2:</h1>";
+
+$query="SELECT ramo.nombre
+FROM ramo, curso, nota
+WHERE ramo.sigla = curso.sigla
+AND curso.nrc = nota.nrc
+AND nota.notafinal >= 4.0
+AND nota.username = '" . $_POST['username'] . "'";
+
+echo '<table border="1">';
+echo '<tr>';
+echo "<th>Ramo</th>";
+echo "</tr>";
+
+foreach($db->query($query) as $row)
+{
+	echo "<tr>";
+	echo "<td>" . $row[0] . "</td>";
+	echo "</tr>";
+}
+
+echo "</table>";
+
+?>
--- Sites/index.php
+++ Sites/index.php
@@ -3,19 +3,19 @@
 <h2>Bienvenido a la mejor pagina de Bases de Datos</h2>
 <h3>Haz click en el link de la consulta que quieras realizar</h3>
 
-Consulta 1
+<h5>Consulta 1</h5>
 <p>Alumnos que reprobaron un curso</p>
 <form action="consultasEntrega3/consulta1.php" method="post">
-<input type="text" name="sigla"/><br>
-<input type="submit"/><br>
-</form>
+<input type="text" name="sigla"/>
+<input type="submit"/>
+</form><br>
 
-Consulta 2
-<p>Alumnos que cumplen los requisitos de un curso y aun no lo han tomado</p>
+<h5>Consulta 2</h5>
+<p>Cursos que ha aprobado un alumno</p>
 <form action="consultasEntrega3/consulta2.php" method="post">
-<input type="text" name="sigla"/><br>
-<input type="submit"/><br>
-</form>
+<input type="text" name="username"/>
+<input type="submit"/>
+</form><br>
 
 <a href="consultasEntrega3/consulta2.php">Consulta 2</a><br>
 <a href="consultasEntrega3/consulta3.php">Consulta 3</a><br>
