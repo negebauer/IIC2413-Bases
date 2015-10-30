343a6687ace856fb9d21bc1d1fb1de705d0c9f37
nicogeb <negebauer@uc.cl>
2015-10-29 22:23:29 -0300

    Consulta 1 nicer

--- Sites/index.php
+++ Sites/index.php
@@ -1,5 +1,6 @@
 <head>
-	
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>RENNAB</title>
 
 </head>
@@ -8,13 +9,6 @@
 
 	<h1>RENNAB</h1>
 	<h3>Realiza tu consulta favorita</h3>
-	
-	<h5>Consulta 1</h5>
-	<p>Alumnos que reprobaron un curso</p>
-	<form action="consultasEntrega3/consulta1.php" method="post">
-	<input type="text" name="sigla"/>
-	<input type="submit"/>
-	</form><br>
 	
 	<h5>Consulta 2</h5>
 	<p>Cursos que ha aprobado un alumno</p>
@@ -31,7 +25,7 @@
 	</form><br>
 	
 	<h5>Consulta 4</h5>
-	<p>Nota minima, maxima, promedio y mediana de un curso dado por cada semestre dictado</p>
+	<p>Nota mínima, máxima, promedio y mediana de un curso dado por cada semestre dictado</p>
 	<form action="consultasEntrega3/consulta4.php" method="post">
 	<input type="text" name="sigla"/>
 	<input type="submit"/>
@@ -51,3 +45,33 @@
 	</form><br>
 
 </body>
+
+<?php
+
+	try {
+		$db = new PDO("pgsql:dbname=grupo5;host=localhost;port=5432;user=grupo5;password=gruponico"); 
+		}
+	catch(PDOException $e) {
+		echo $e->getMessage();
+		}
+
+	$datosConsulta1="SELECT curso.sigla, curso.ano, curso.semestre
+	FROM curso";
+	
+	echo "<h5>Consulta 1</h5>";
+	echo "<p>Alumnos que reprobaron un curso</p>";
+	echo "<form action="consultasEntrega3/consulta1.php" method="post">";
+	echo "<label><select name="sigla">";
+
+	$i = 0;
+	foreach($db->query($query) as $row)
+	{
+		echo "<option value="0">'$row[0]' '$row[1]' '$row[2]'</option>";
+		$i = $i + 1;
+	}
+
+	echo "</label>";
+	echo "<input type="submit"/>";
+	echo "</form><br>";
+
+?>
