de35e3263c70192d51cfb318c313696507632f06
figarrido <fgarridob.95@gmail.com>
2015-11-26 14:59:35 -0300

    Probando 2

--- Sites/login.php
+++ Sites/login.php
@@ -9,8 +9,8 @@
 
 function verificar_login($user,$password,&$result) 
     { 
-        $sql = “SELECT * FROM usuarios WHERE usuario = ‘$user’ and ‘$password’ = ‘$password’”; 
-        $rec = mysql_query($sql); 
+        $sql = “SELECT * FROM usuario WHERE username = ‘$user’ and password = ‘$password’”; 
+        $rec = query($sql); 
         $count = 0; 
         while($row = mysql_fetch_object($rec)) 
         { 
