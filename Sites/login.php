46dac89ae19094d46c910c559bc5bfd547f12575
figarrido <fgarridob.95@gmail.com>
2015-11-26 17:07:05 -0300

    Probando nuevamente

--- Sites/login.php
+++ Sites/login.php
@@ -1,48 +1,31 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> f71aa51b651a94298843fe67b48e6ba17b2f7bdf
 <?php
 
-	try {
-		$db = new PDO("pgsql:dbname=grupo5;host=localhost;port=5432;user=grupo5;password=gruponico"); 
-<<<<<<< HEAD
-		$message = "Me conecté!!! Yujuuu!!!";
-		echo "<script type='text/javascript'>alert('$message');</script>";
-=======
->>>>>>> f71aa51b651a94298843fe67b48e6ba17b2f7bdf
-		}
-	catch(PDOException $e) {
-		echo $e->getMessage();
-		}
+    try {
+        $db = new PDO("pgsql:dbname=grupo5;host=localhost;port=5432;user=grupo5;password=gruponico"); 
+        $message = "Me conecté!!! Yujuuu!!!";
+        echo "<script type='text/javascript'>alert('$message');</script>";
+        }
+    catch(PDOException $e) {
+        echo $e->getMessage();
+        }
 
 function verificar_login($user,$password,&$result) 
-<<<<<<< HEAD
     { 
-	    $sql = 'SELECT * FROM usuario WHERE username = \'$user\' and password = \'$password\''; 
+        $sql = 'SELECT * FROM usuario WHERE username = \'$user\' and password = \'$password\''; 
         $rec = $db -> query($sql); 
-=======
-    {
-        $sql = 'SELECT * FROM usuario WHERE username = \'$user\' and password = \'$password\'';
-        $rec = $db -> query($db, $sql);
->>>>>>> f71aa51b651a94298843fe67b48e6ba17b2f7bdf
         if (!$rec) {
-        	return 0;
+            return 0;
         } else {
-        	return 1;
+            return 1;
         }
-<<<<<<< HEAD
         
     } 
-=======
-    }
->>>>>>> f71aa51b651a94298843fe67b48e6ba17b2f7bdf
 
 /*Luego haremos una serie de condicionales que identificaran el momento en el boton de login es presionado y cuando este sea presionado llamaremos a la función verificar_login() pasandole los parámetros ingresados:*/
 
 if(!isset($_SESSION['userid'])) //para saber si existe o no ya la variable de sesión que se va a crear cuando el usuario se logee
 { 
-    if(isset($_POST['login'])) //Si la primera condición no pasa, haremos otra preguntando si el boton de login 	fue presionado
+    if(isset($_POST['login'])) //Si la primera condición no pasa, haremos otra preguntando si el boton de login     fue presionado
     { 
         if(verificar_login($_POST['user'],$_POST['password'],$result) == 1) //Si el boton fue presionado llamamos a la función verificar_login() dentro de otra condición preguntando si resulta verdadero y le pasamos los valores ingresados como parámetros.
         { 
@@ -67,63 +50,4 @@ if(!isset($_SESSION['userid'])) //para saber si existe o no ya la variable de se
     echo 'Su usuario ingreso correctamente.'; 
     echo '<a href="logout.php">Logout</a>'; 
 } 
-<<<<<<< HEAD
 ?> 
-=======
-?> 
-
-<h1>Hola</h1>
-
-// <!--?php
-
-// 	try {
-// 		$db = new PDO("pgsql:dbname=grupo5;host=localhost;port=5432;user=grupo5;password=gruponico"); 
-// 		}
-// 	catch(PDOException $e) {
-// 		echo $e->getMessage();
-// 		}
-
-// function verificar_login($user,$password,&$result) 
-//     { 
-//         $sql = “SELECT * FROM usuario WHERE username = ‘$user’ and password = ‘$password’”; 
-//         $rec = query($sql); 
-//         if (!$rec) {
-//         	return 0;
-//         } else {
-//         	return 1;
-//         }
-        
-//     } 
-
-// /*Luego haremos una serie de condicionales que identificaran el momento en el boton de login es presionado y cuando este sea presionado llamaremos a la función verificar_login() pasandole los parámetros ingresados:*/
-
-// if(!isset($_SESSION['userid'])) //para saber si existe o no ya la variable de sesión que se va a crear cuando el usuario se logee
-// { 
-//     if(isset($_POST['login'])) //Si la primera condición no pasa, haremos otra preguntando si el boton de login fue presionado
-//     { 
-//         if(verificar_login($_POST['user'],$_POST['password'],$result) == 1) //Si el boton fue presionado llamamos a la función verificar_login() dentro de otra condición preguntando si resulta verdadero y le pasamos los valores ingresados como parámetros.
-//         { 
-//             /*Si el login fue correcto, registramos la variable de sesión y al mismo tiempo refrescamos la pagina index.php.*/ 
-//             $_SESSION['userid'] = $result->idusuario; 
-//             header("location:index.php"); 
-//         } 
-//         else 
-//         { 
-//             echo '<div class="error">Su usuario es incorrecto, intente nuevamente.</div>'; //Si la función verificar_login() no pasa, que se muestre un mensaje de error. 
-//         } 
-//     } 
-// ?> 
-// <form action="" method="post" class="login"> 
-//     <div><label>Username</label><input name="user" type="text" ></div> 
-//     <div><label>Password</label><input name="password" type="password"></div> 
-//     <div><input name="login" type="submit" value="login"></div> 
-// </form> 
-// <?php 
-// } else { 
-//     // Si la variable de sesión ‘userid’ ya existe, que muestre el mensaje de saludo. 
-//     echo 'Su usuario ingreso correctamente.'; 
-//     echo '<a href="logout.php">Logout</a>'; 
-// } 
-// ?--> 
-
->>>>>>> f71aa51b651a94298843fe67b48e6ba17b2f7bdf
