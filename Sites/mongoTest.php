849a2c1110c4e500e00f82d8564ed1bd0fa4d008
nicogeb <negebauer@uc.cl>
2015-11-28 02:55:13 -0300

    Try mongo 7

--- Sites/mongoTest.php
+++ Sites/mongoTest.php
@@ -1,33 +1,33 @@
 <?php
 
 echo "<h3>Connecting</h3>";
-echo <br>;
+echo "<br>";
 echo $dbhost = "localhost";
-echo <br>;
+echo "<br>";
 echo $dbname = "test";
-echo <br>;
+echo "<br>";
 echo $mongo = new MongoClient("mongodb://$dbhost");
-echo <br>;
+echo "<br>";
 echo "<h3>Setting database reference</h3>";
-echo <br>;
+echo "<br>";
 echo $db = $mongo->$dbname;
 echo "<h3>Setting collections references</h3>";
-echo <br>;
+echo "<br>";
 echo $db = $mongo->$dbname;
-echo <br>;
+echo "<br>";
 echo $alumnos = $db->alumnos;
-echo <br>;
+echo "<br>";
 echo $cursos = $db->cursos;
-echo <br>;
+echo "<br>";
 echo $universidades = $db->universidades;
-echo <br>;
+echo "<br>";
 echo "<h3>Showing stuff</h3>";
-echo <br>;
+echo "<br>";
 echo $alumnos->find();
-echo <br>;
+echo "<br>";
 echo $cursos->find();
-echo <br>;
+echo "<br>";
 echo $universidades->find();
-echo <br>;
+echo "<br>";
 
 ?>
