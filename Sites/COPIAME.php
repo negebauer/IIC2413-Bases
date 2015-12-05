6f98ed900b357716694d575f0aae53751839f96b
negebauer <negebauer@uc.cl>
2015-12-04 23:51:10 -0300

    Agregado archivo molde para paginas php

---
+++ Sites/COPIAME.php
@@ -0,0 +1,16 @@
+<?php
+
+// #################### DECLARACION BASES DE DATOS ####################
+$dbhost = "localhost";
+$dbname = "test";
+$mongo = new MongoClient("mongodb://$dbhost");
+$dbm = $mongo->$dbname;
+
+try {
+	$dbp = new PDO("pgsql:dbname=grupo5;host=localhost;port=5432;user=grupo5;password=gruponico");
+}
+catch(PDOException $e) {
+	echo $e->getMessage();
+}
+
+?>
