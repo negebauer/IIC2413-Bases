4ccc90bfc77f113084042ec9685bd6d221b0ef40
negebauer <negebauer@uc.cl>
2015-12-05 00:38:52 -0300

    #20 Informacion alumno

--- Sites/informacionAlumno.php
+++ Sites/informacionAlumno.php
@@ -39,26 +39,16 @@ foreach ($alumnosRowArray as $alumno) {
 	array_push($alumnos, $alumno[0]);
 }
 
-echo "Admins: $admins<br><br>";
-echo "Alumnos: $alumnos<br><br>";
-
 if (in_array($username, $admins)) {
 	$esAdmin = true;
 } elseif (in_array($username, $alumnos)) {
 	$esAlumno = true;
 }
 
-$boolAlumnoRaw = $esAlumno ? 'true' : 'false';
-$boolAdminRaw = $esAdmin ? 'true' : 'false';
-echo "Es alumno: $boolAlumnoRaw";
-echo "Es admin: $boolAdminRaw";
-
 // #################### AHORA A HACER MAGIA ####################
-
 if ($esAlumno || $esAdmin) {
-	// Declaramos las consultas
-	$queryInfoAlumno = "SELECT usuario.rut, usuario.username, usuario.nombres, usuario.apellidop, usuario.apellidom, alumno.mailuc, usuario.paisorigen,
-							usuario.sexo, usuario.estadocivil, usuario.fnacimiento, alumno.anoadmin, alumno.encausal 
+	// ##### Declaramos las consultas #####
+	$queryInfoAlumno = "SELECT usuario.rut, usuario.username, usuario.nombres, usuario.apellidop, usuario.apellidom, alumno.mailuc, alumno.anoadmin, alumno.encausal 
 						FROM usuario, alumno
 						WHERE usuario.username = alumno.username
 						AND alumno.username = '{$usernameAlumno}';";
@@ -69,6 +59,69 @@ if ($esAlumno || $esAdmin) {
 						AND nota.nrc = curso.nrc
 						AND ramo.sigla = curso.sigla
 						ORDER BY curso.ano, curso.semestre, curso.sigla, ramo.nombre;";
+
+	// ##### Hacemos las consultas #####
+	$informacionAlumnoRowArray = $dbp->query($queryAdmins)->fetchAll();
+	$cursosRowArray = $dbp->query($queryAlumnos)->fetchAll();
+
+	// ##### Tabla información alumno #####
+	echo '<table border="1" class="table">';
+	echo '<tr>';
+	echo "<th>RUT</th>";
+	echo "<th>Usuario</th>";
+	echo "<th>Nombres</th>";
+	echo "<th>Apellido Paterno</th>";
+	echo "<th>Apellido Materno</th>";
+	echo "<th>Mail UC</th>";
+	echo "<th>Año admision</th>";
+	echo "<th>En causal</th>";
+	echo "</tr>";
+	
+	foreach($informacionAlumnoRowArray as $informacionAlumno)
+	{
+		echo "<tr>";
+		echo "<td>" . $informacionAlumno[0] . "</td>";
+		echo "<td>" . $informacionAlumno[1] . "</td>";
+		echo "<td>" . $informacionAlumno[2] . "</td>";
+		echo "<td>" . $informacionAlumno[3] . "</td>";
+		echo "<td>" . $informacionAlumno[4] . "</td>";
+		echo "<td>" . $informacionAlumno[5] . "</td>";
+		echo "<td>" . $informacionAlumno[6] . "</td>";
+		echo "<td>" . $informacionAlumno[7] . "</td>";
+		echo "</tr>";
+	}
+	
+	echo "</table>";
+	echo "<br><br>";
+
+	// ##### Tabla información cursos del alumno #####
+	echo '<table border="1" class="table">';
+	echo '<tr>';
+	echo "<th>NRC</th>";
+	echo "<th>Sigla</th>";
+	echo "<th>Seccion</th>";
+	echo "<th>Nombre ramo</th>";
+	echo "<th>Año</th>";
+	echo "<th>Semestre</th>";
+	echo "<th>Nota</th>";
+	echo "</tr>";
+	
+	foreach($cursosRowArray as $curso)
+	{
+		echo "<tr>";
+		echo "<td>" . $curso[0] . "</td>";
+		echo "<td>" . $curso[1] . "</td>";
+		echo "<td>" . $curso[2] . "</td>";
+		echo "<td>" . $curso[3] . "</td>";
+		echo "<td>" . $curso[4] . "</td>";
+		echo "<td>" . $curso[5] . "</td>";
+		echo "<td>" . $curso[6] . "</td>";
+		echo "</tr>";
+	}
+	
+	echo "</table>";
+	echo "<br><br>";
+
 }
 
 
