2d6d63339ed5832b9a53a1720c9ff67d8cf25eb8
negebauer <negebauer@uc.cl>
2015-12-05 01:22:42 -0300

    Modificado COPIAME para usar nueva manera de ver usuario

--- Sites/COPIAME.php
+++ Sites/COPIAME.php
@@ -21,7 +21,7 @@ catch(PDOException $e) {
 	echo $e->getMessage();
 }
 
-// #################### VARIABLES POR DESIGNAR ####################
+// #################### VARIABLES ####################
 $username = "testuser1";			// username de quien hace la consulta
 $usernameAlumno = "testuser1";		// username del alumno sobre el cual se hace la consulta. Si user es alumno entonces (username = usernameAlumno).
 $esAdmin = false;					// si la consulta la hace un admin
@@ -31,60 +31,10 @@ $esProfesor = false;				// si la consulta la hace un profesor
 
 
 // #################### VERIFICAR USUARIO ####################
-$queryAdmins = "SELECT username
-				FROM administrador;";
-
-$queryAlumnos = "SELECT username
-				FROM alumno;";
-
-$queryAlumnosIntercambio = "SELECT username
-							FROM alumnointercambio;";
-
-$queryProfesores = "SELECT username
-					FROM profesor;";
-
-$adminsRowArray = $dbp->query($queryAdmins)->fetchAll();
-$alumnosRowArray = $dbp->query($queryAlumnos)->fetchAll();
-$alumnosIntercambioRowArray = $dbp->query($queryAlumnosIntercambio)->fetchAll();
-$profesoresRowArray = $dbp->query($queryProfesores)->fetchAll();
-
-$admins = [];
-$alumnos = [];
-$alumnosIntercambio = [];
-$profesores = [];
-
-foreach ($adminsRowArray as $admin)
-{
-	array_push($admins, $admin[0]);
-}
-foreach ($alumnosRowArray as $alumno)
-{
-	array_push($alumnos, $alumno[0]);
-}
-foreach ($alumnosIntercambioRowArray as $alumnoIntercambio)
-{
-	array_push($alumnosIntercambio, $alumnoIntercambio[0]);
-}
-foreach ($profesoresRowArray as $profesor)
-{
-	array_push($profesores, $profesor[0]);
-}
-
-if (in_array($username, $admins))
-{
-	$esAdmin = true;
-}
-elseif (in_array($username, $alumnos))
-{
-	$esAlumno = true;
-}
-elseif (in_array($username, $alumnosIntercambio))
-{
-	$esAlumnoIntercambio = true;
-}
-elseif (in_array($username, $profesores))
-{
-	$esProfesor = true;
-}
+$arrayEsUsuario = verificarUsuario();
+$esAdmin = $arrayEsUsuario[0];
+$esAlumno = $arrayEsUsuario[1];
+$esAlumnoIntercambio = $arrayEsUsuario[2];
+$esProfesor = $arrayEsUsuario[3];
 
 ?>
