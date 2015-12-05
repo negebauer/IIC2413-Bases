25603d5fe1a3d8f631e5b19055eef01dbcc5524b
negebauer <negebauer@uc.cl>
2015-12-05 00:40:00 -0300

    Agregado head a COPIAME

--- Sites/COPIAME.php
+++ Sites/COPIAME.php
@@ -1,3 +1,11 @@
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>RENNAB</title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+</head>
+
 <?php
 
 // #################### DECLARACION BASES DE DATOS ####################
