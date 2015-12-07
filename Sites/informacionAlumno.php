2d528de1a98f4206a27928cb84463093ef35f1d6
negebauer <negebauer@uc.cl>
2015-12-07 01:08:11 -0300

    Nueva manera de mostrar informacion alumno 2

--- Sites/informacionAlumno.php
+++ Sites/informacionAlumno.php
@@ -12,25 +12,20 @@
 require_once('functions.php');
 
 // #################### VARIABLES ####################
-$usernameAlumno = "";		// username del alumno sobre el cual se hace la consulta.
-							// Si user es alumno entonces (username = usernameAlumno).
+$usernameAlumno = $_POST['usernameAlumno'];	
 
 // #################### AHORA A HACER MAGIA ####################
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   $usernameAlumno = $_POST['usernameAlumno'];	
-}
-
 if ($esAlumno || $esAdmin || $esProfesor)
 {
 	// ##### Declaramos las consultas #####
 	$queryInfoAlumno = "SELECT usuario.rut, usuario.username, usuario.nombres, usuario.apellidop, usuario.apellidom, alumno.mailuc, alumno.anoadmin, alumno.encausal 
 						FROM usuario, alumno
 						WHERE usuario.username = alumno.username
-						AND alumno.username = '{$usernameAlumno}';";
+						AND alumno.username = '$usernameAlumno';";
 	
 	$queryCursosAlumno = "SELECT curso.nrc, curso.sigla, curso.seccion, ramo.nombre, curso.ano, curso.semestre, nota.notafinal
 						FROM nota, ramo, curso
-						WHERE nota.username = '{$usernameAlumno}'
+						WHERE nota.username = '$usernameAlumno'
 						AND nota.nrc = curso.nrc
 						AND ramo.sigla = curso.sigla
 						ORDER BY curso.ano, curso.semestre, curso.sigla, ramo.nombre;";
