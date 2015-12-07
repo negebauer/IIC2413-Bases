cc93da1f0e22292310350a93421d11f852fd22ae
figarrido <fgarridob.95@gmail.com>
2015-12-07 03:05:45 -0300

    Info intercambio solo puede acceder admin

--- Sites/informacionAlumnoIntercambio.php
+++ Sites/informacionAlumnoIntercambio.php
@@ -29,91 +29,96 @@ require_once('functions.php');
 // 			y el nombre de la variables que se enviara en POST sera $posstVarName
 
 // #################### AHORA A HACER MAGIA ####################
-
-$id = $_POST["usernameAlumno"];
-$alumnos = $dbm->alumnos;
-$universidades = $dbm->universidades;
-$cursos = $dbm->cursos;
-
-$mongoid = new MongoId($id);
-$idQuery = array("_id" => $mongoid);
-$alumnosMatch = $alumnos->find($idQuery);
-$alumnosMatch->next();
-$alumno = $alumnosMatch->current();
-
-$columnas = array(
-	"Nombre",
-	"Apellido",
-	"Mail",
-	"Universidad"
-	);
-
-foreach (iterator_to_array($universidades->find()) as $universidad)
+if ($esAdmin)
 {
-	if ($universidad["_id"] == $alumno["universidad"]) {
-    	$universidadAlumno = $universidad["nombre"];
-    	break;
-	}
-}
-$splited = explode(" ", $alumno["nombre"]);
-imprimirTabla($columnas, array(array(
-										$splited[0],
-										$splited[1],
-										$alumno["email"],
-										$universidadAlumno
-										)));
+	$id = $_POST["usernameAlumno"];
+	$alumnos = $dbm->alumnos;
+	$universidades = $dbm->universidades;
+	$cursos = $dbm->cursos;
 
-$columnasCursos = array(
-	"Curso aprobado",
-	"Sigla UC",
-	"Curso UC equivalente");
+	$mongoid = new MongoId($id);
+	$idQuery = array("_id" => $mongoid);
+	$alumnosMatch = $alumnos->find($idQuery);
+	$alumnosMatch->next();
+	$alumno = $alumnosMatch->current();
 
-$aprobados = array();
+	$columnas = array(
+		"Nombre",
+		"Apellido",
+		"Mail",
+		"Universidad"
+		);
 
-foreach (iterator_to_array($cursos->find()) as $curso)
-{
-	if (in_array($curso["_id"], $alumno["cursos"]))
+	foreach (iterator_to_array($universidades->find()) as $universidad)
 	{
-		array_push($aprobados, array(
-			"_id" => $curso["_id"],
-			"nombre" => $curso["nombre"],
-			"equivalencia" => $curso["equivalencia"]
-			));
+		if ($universidad["_id"] == $alumno["universidad"]) {
+	    	$universidadAlumno = $universidad["nombre"];
+	    	break;
+		}
 	}
-}
+	$splited = explode(" ", $alumno["nombre"]);
+	imprimirTabla($columnas, array(array(
+											$splited[0],
+											$splited[1],
+											$alumno["email"],
+											$universidadAlumno
+											)));
 
-$datos = array();
+	$columnasCursos = array(
+		"Curso aprobado",
+		"Sigla UC",
+		"Curso UC equivalente");
 
-foreach ($aprobados as $curso)
-{
-	$query = "SELECT sigla, nombre
-			FROM ramo
-			WHERE sigla = '{$curso['equivalencia']}'
-			AND nombre <> '{$curso['nombre']}'
-			AND escuela <> 'EscuelaNoIdentificada'";
-	$queryResult = $dbp->query($query);
-	$notFound = true;
-	$k = 0;
-	foreach ($queryResult as $row)
+	$aprobados = array();
+
+	foreach (iterator_to_array($cursos->find()) as $curso)
 	{
-		echo $k += 1;
-		array_push($datos, array(
-			$curso["nombre"],
-			$row[0],
-			$row[1]
-			));
-		$notFound = false;
+		if (in_array($curso["_id"], $alumno["cursos"]))
+		{
+			array_push($aprobados, array(
+				"_id" => $curso["_id"],
+				"nombre" => $curso["nombre"],
+				"equivalencia" => $curso["equivalencia"]
+				));
+		}
 	}
-	if ($notFound)
-	{
-		array_push($datos, array(
-			$curso["nombre"],
-			"--",
-			"--"
-			));
-	}
-}
 
-imprimirTabla($columnasCursos, $datos);
+	$datos = array();
+
+	foreach ($aprobados as $curso)
+	{
+		$query = "SELECT sigla, nombre
+				FROM ramo
+				WHERE sigla = '{$curso['equivalencia']}'
+				AND nombre <> '{$curso['nombre']}'
+				AND escuela <> 'EscuelaNoIdentificada'";
+		$queryResult = $dbp->query($query);
+		$notFound = true;
+		$k = 0;
+		foreach ($queryResult as $row)
+		{
+			echo $k += 1;
+			array_push($datos, array(
+				$curso["nombre"],
+				$row[0],
+				$row[1]
+				));
+			$notFound = false;
+		}
+		if ($notFound)
+		{
+			array_push($datos, array(
+				$curso["nombre"],
+				"--",
+				"--"
+				));
+		}
+	}
+
+	imprimirTabla($columnasCursos, $datos);
+}
+else{
+	header('location:index.php');
+}
 
 ?>
